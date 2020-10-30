58b8618cbc7a02aff42a318e5fa3cc5733113192
andreia23 <andreialima.berto08@gmail.com>
2020-10-30 17:45:18 -0300

    feat: criando service para pedido

---
+++ src/app/shared/services/pedido.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {Pedido} from '../model/pedido';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  URL_PEDIDOS =  'http://localhost:3000/pedidos';
+  pedidos: Array<Pedido>;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  listar(): Observable<Pedido[]>{
+    return  this.httpClient.get<Pedido[]>( this.URL_PEDIDOS );
+  }
+
+  inserir(pedido: Pedido): Observable<Pedido>{
+    return this.httpClient.post<Pedido>(this.URL_PEDIDOS, pedido);
+  }
+  remover(id: number): Observable<object> {
+    return this.httpClient.delete(`${this.URL_PEDIDOS}/${id}`);
+  }
+
+  pesquisarPorId(id: number): Observable<Pedido> {
+    return this.httpClient.get<Pedido>(`${this.URL_PEDIDOS}/${id}`);
+  }
+
+  atualizar(pedido: Pedido): Observable<Pedido> {
+    return this.httpClient.put<Pedido>(`${this.URL_PEDIDOS}/${pedido.id}`, pedido);
+  }
+}

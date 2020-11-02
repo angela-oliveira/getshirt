045b018abc0a0f1aff38d30c471027830093c9ca
Helena <helenajuliana14@gmail.com>
2020-11-02 18:08:52 -0300

    Criar módulo cliente e componente cadastrar

---
+++ src/app/cliente/cliente.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastrarClienteComponent } from './cadastrar-cliente/cadastrar-cliente.component';
+
+
+
+@NgModule({
+  declarations: [CadastrarClienteComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClienteModule { }

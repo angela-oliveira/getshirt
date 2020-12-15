29bd33bf7fe6b09b6d05e1f9b341bcbd7762520a
andreia23 <andreialima.berto08@gmail.com>
2020-12-15 00:57:55 -0300

    Componente empresa

---
+++ src/app/empresa/cadastrar-empresa/cadastrar-empresa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-empresa',
+  templateUrl: './cadastrar-empresa.component.html',
+  styleUrls: ['./cadastrar-empresa.component.scss']
+})
+export class CadastrarEmpresaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/empresa/empresa.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastrarEmpresaComponent } from './cadastrar-empresa/cadastrar-empresa.component';
+
+
+
+@NgModule({
+  declarations: [CadastrarEmpresaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmpresaModule { }
